4999cfa1acaa12cd607fe8b7b91115c00b45314f
Marburg1234 <172342315+Marburg1234@users.noreply.github.com>
2024-08-04 03:27:09 +0000

    Update calculation_practice

--- app.js
+++ app.js
@@ -1,1 +1,83 @@
-alert('Hello World');
+
+// 繰り返し処理 for文→繰り返し処理の回数を決める
+let i;
+let num = 0;
+let limit = 500
+
+for(i = 1; i < limit; i++){
+  num = num + i;
+}
+
+alert ('1から' + limit + 'まで足し算した結果は' + num + 'です');
+
+// 繰り返し処理 while文→trueである限り処理を実行し続ける
+// let max = 1000;
+// let num = 1;
+// let count = 0;
+
+// while (num < max){
+//   num = num * 2;
+//   count = count + 1;
+// }
+
+// alert('2を掛け続けて' + max + 'を超えるのに必要だった回数は' + count + '回です');
+
+// 条件分岐を使って出力までしてみる
+// let orange = 30000;
+// let apple = 120;
+
+// if(orange < apple){
+//   alert('みかんの値段がりんごより安い');
+// } else if (orange == apple){
+//   alert('みかんとりんごは同じ値段');
+// } else {
+//   alert('みかんの値段がりんごより高い');
+// }
+
+// 変数を利用して、変数同士を連結させて文字列を表示する
+// let word1 = 'You are ';
+// let word2 = 'Amazing!!';
+// alert(word1 + word2);
+
+// 文字列を連結させてみる
+// alert('Hello' + ' World');
+
+// 変数に計算結果を入れて出力してみる
+// let cal = (8 + 3);
+// alert(cal)
+
+// 四則計算で結果を表示する
+// alert(8 + 3);
+// alert(8 * 5);
+// alert(8 / 4);
+// alert(2*6);
+
+
+// 足し算をしてくれる
+// alert(4 + 3);
+
+// 文字列を代入して、変数として処理→文字列を表示する
+// let word = 'Congraturation!! You hava amazing skill!';
+// alert(word);
+
+// // 変数の代入練習
+// let int1 = 1;
+// alert(int1);
+
+// let int2 = 2;
+// alert(int2);
+
+// let int3 = 3;
+// alert(int3);
+
+// let str1 = 'Javascriptを覚えよう';
+
+// 変数代入と出力する
+// let int1 = 1;
+// alert(int1)
+
+// let hello = 'Hello World';
+// alert(hello);
+
+// 任意のものをアラートとして表示してくれる機能 alert( )
+// alert('Hello World');
